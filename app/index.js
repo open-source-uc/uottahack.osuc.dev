0c0938bedcfe008e917886c0f1cb68e237dad6a4
Emmanuel Norambuena <eanorambuena@uc.cl>
2024-01-06 15:12:26 -0300

    feat: add baticristo

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import { load, html } from 'emmy-dom/dist/server.js'
 
 export function app () {
-  this.className = 'flex flex-col justify-center items-center text-center w-full h-fit text-gray-800 gap-0'
+  this.className = 'flex flex-col justify-center items-left w-full h-fit text-gray-800 gap-0 relative'
 
   this.useEffect(() => {
     this.querySelector('#cta').addEventListener('click', () => {
@@ -11,17 +11,19 @@ export function app () {
   }, ['didMount'])
 
   return html`
+    <img class="absolute top-[450px] lg:top-[320px] xl:scale-150 2xl:scale-[3] left-[50px]" src="sagrado_corazon.png" alt="Sagrado Corazón" width="100px">
     <section class="h-[900px] w-screen bg-cover bg-[url('https://2024.uottahack.ca/images/CompleteBackground.png')]">
-      <article class="h-screen w-full flex flex-col justify-center items-center gap-6 text-white">  
+      <article class="h-screen w-full flex flex-col justify-center items-left gap-6 text-white">  
         <h1 class="text-4xl sm:text-7xl md:text-[6rem] lg:text-[8rem] font-bold font-['Harabara'] drop-shadow-lg">
           uOttaHack 6 UC
         </h1>
         <p class="text-2xl md:text-4xl">
-          1 a 3 de marzo
+          1 a 3 de marzo, Campus San Joaquín UC
         </p>
+        <!--
         <button id="cta" class="bg-[#384DBF] text-white text-2xl md:text-4xl px-4 py-2 rounded-lg drop-shadow-lg">
           Ver más
-        </button>
+        </button>-->
       </article>
     </section>
     <section class="h-screen w-screen flex flex-col justify-center items-center gap-4">
