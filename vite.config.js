9623216ce1216da79c326dc2eaf7e5932adb9b38
Emmanuel Norambuena <eanorambuena@uc.cl>
2024-01-06 04:53:39 -0300

    chore: update vite.config.js to specify input files for rollupOptions

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,14 @@
-export default {
-  base: '/'
-}
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'login.html'),
+        signup: resolve(__dirname, 'signup.html')
+      }
+    }
+  }
+})
