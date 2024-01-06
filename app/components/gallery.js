cc36706dc465c65c869f16f3dca9c60fe944b5dd
Emmanuel Norambuena <eanorambuena@uc.cl>
2024-01-06 16:38:42 -0300

    feat: refactor gallery component to dynamically generate image elements

--- app/components/gallery.js
+++ app/components/gallery.js
@@ -2,44 +2,22 @@ import { load, html } from 'emmy-dom/dist/server.js'
 
 export function gallery () {
   this.className = 'flex flex-col justify-center items-center text-center w-[80%] h-fit text-gray-800 gap-0'
+  const srcs = []
+  const imgs = [71, 74, 79, 73, 75, 76, 77, 78, 80, 81, 72]
+  imgs.forEach(i => {
+    srcs.push(`pics/photo_51435864726061504${i}_y.jpg`)
+  })
   return html`
     <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
-      <div>
-          <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" alt="">
-      </div>
-      <div>
-          <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt="">
-      </div>
-      <div>
-          <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt="">
-      </div>
-      <div>
-          <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt="">
-      </div>
-      <div>
-          <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg" alt="">
-      </div>
-      <div>
-          <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg" alt="">
-      </div>
-      <div>
-          <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg" alt="">
-      </div>
-      <div>
-          <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg" alt="">
-      </div>
-      <div>
-          <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg" alt="">
-      </div>
-      <div>
-          <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg" alt="">
-      </div>
-      <div>
-          <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg" alt="">
-      </div>
-      <div>
-          <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg" alt="">
-      </div>
+    ${
+        srcs.map(src => {
+            return html`
+                <div class="rounded-lg">
+                    <img class="object-cover rounded-lg" src="${src}" alt="Codefest 2023">
+                </div>
+            `
+        }).join('')
+    }
     </div>
   `
 }
